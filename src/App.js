a7f924a767d94a64d0396df9c98e1a04a59c39b5
Terry Bu <terry.bu@verizonwireless.com>
2017-09-12 18:42:51 -0400

    Showing button to change state upon button click

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonActive: false,
+      buttonTextStart: "Start Spinner",
+    };
+    this.clicked = this.clicked.bind(this);
+  }
+
+  clicked() {
+    this.state.butonActive = !this.state.buttonActive;
+    this.setState( {buttonActive: this.state.butonActive });
+    if (this.state.butonActive) {
+      console.log("set to stop spinner");
+      this.setState({ buttonTextStart: "Stop Spinner"});
+    } else {
+      console.log("set to start spinner");
+      this.setState({ buttonTextStart: "Start Spinner" });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>ReactJS POC</h2>
         </div>
         <p className="App-intro">
-          ReactJS POC
+
         </p>
-        <button>Click to start Spinner</button>
+        <button onClick={ (e) => { this.clicked(); } }>{this.state.buttonTextStart}</button>
       </div>
 
     );
   }
 }
-
 
 export default App;
